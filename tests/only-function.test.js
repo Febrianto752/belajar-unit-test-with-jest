ca7fb833a752396142db96285253ddb3cb1d2e86
Febrianto <febrianto.bekasi@gmail.com>
2023-05-05 17:31:14 +0700

    learn only function

---
+++ tests/only-function.test.js
@@ -0,0 +1,11 @@
+it.only("should 1 + 1 equal to 2", () => {
+  expect(1 + 1).toEqual(2);
+});
+
+it("should 2 + 2 equal to 4", () => {
+  expect(2 + 2).toEqual(4);
+});
+
+it("should 3 + 3 equal to 6", () => {
+  expect(3 + 3).toEqual(6);
+});
